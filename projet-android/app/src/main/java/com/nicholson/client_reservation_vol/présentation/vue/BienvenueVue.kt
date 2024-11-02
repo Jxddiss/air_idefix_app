ed9aa70e06a6e790f25cb835083ecb8495330f78
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-01 21:41:58 -0400

    Implémentation de la vue de Bienvenue

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/vue/BienvenueVue.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/vue/BienvenueVue.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.nicholson.client_reservation_vol.R
+import com.nicholson.client_reservation_vol.présentation.présentateur.bienvenue.BienvenuePrésentateur
+import com.nicholson.client_reservation_vol.présentation.présentateur.bienvenue.ContratVuePrésentateurBienvenue.*
 
-class BienvenueVue : Fragment() {
+class BienvenueVue : Fragment(), IBienvenueVue {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var présentateur : IBienvenuePrésentateur? = BienvenuePrésentateur( this )
+    lateinit var btnGoMesVoyages : ImageButton
+    lateinit var btnGoRechercherUnVol : ImageButton
+    lateinit var navController : NavController
+
+    override fun onCreate( savedInstanceState: Bundle? ) {
         super.onCreate(savedInstanceState)
     }
 
@@ -18,6 +28,35 @@ class BienvenueVue : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bienvenue_vue, container, false)
+        return inflater.inflate( R.layout.fragment_bienvenue_vue, container, false )
+    }
+
+    override fun onViewCreated( vue: View, savedInstanceState: Bundle? ) {
+        super.onViewCreated( vue, savedInstanceState )
+        navController = Navigation.findNavController( vue )
+        attacherÉcouteurRedirectionListeReservations( vue )
+        attacherÉcouteurRedirectionRechercherUnVol( vue )
+    }
+
+    override fun redirigerAListeReservation() {
+        navController.navigate( R.id.action_bienvenueVue_vers_listeRéservationsVue )
+    }
+
+    override fun redirigerARechercherUnVol() {
+        navController.navigate( R.id.action_bienvenueVue_vers_rechercherUnVolVue )
+    }
+
+    private fun attacherÉcouteurRedirectionListeReservations( vue : View ){
+        btnGoMesVoyages = vue.findViewById( R.id.imageButtonMesVoyages )
+        btnGoMesVoyages.setOnClickListener {
+            présentateur?.traiterDemandeRedirectionListeReservations()
+        }
+    }
+
+    private fun attacherÉcouteurRedirectionRechercherUnVol( vue : View ){
+        btnGoRechercherUnVol = vue.findViewById( R.id.imageButtonRechercheVols )
+        btnGoRechercherUnVol.setOnClickListener {
+            présentateur?.traiterDemandeRedirectionRechercherUnVol()
+        }
     }
 }
