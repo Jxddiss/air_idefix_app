b43b1053a99d8ac124be2f85a21bc52c7c670713
Rami <2136995@crosemont.qc.ca>
2024-11-03 22:46:32 -0500

    creation model

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/ListeReservation/ListeRéservationsVue.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/ListeReservation/ListeRéservationsVue.kt
@@ -1,16 +1,26 @@
-package com.nicholson.client_reservation_vol.présentation.vue
+package com.nicholson.client_reservation_vol.présentation.ListeReservation
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.nicholson.client_reservation_vol.R
+import com.nicholson.client_reservation_vol.domaine.entité.Réservation
 
 class ListeRéservationsVue : Fragment() {
 
+    lateinit var recycler: RecyclerView
+    lateinit var reservationList: ArrayList<Réservation>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -20,4 +30,16 @@ class ListeRéservationsVue : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_liste_reservations_vue, container, false)
     }
+
+    override fun onViewCreated(vue: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(vue, savedInstanceState)
+
+        
+
+        recycler = vue.findViewById(R.id.recyclerVoyages)
+
+
+    }
+
+
 }
