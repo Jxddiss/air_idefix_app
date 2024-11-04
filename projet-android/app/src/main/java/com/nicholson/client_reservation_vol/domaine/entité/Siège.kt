ab77fceb6d00877995c68813b6337b75252ea893
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-04 12:59:34 -0500

    Corrections des entitées

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/domaine/entité/Siège.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/domaine/entité/Siège.kt
@@ -1,10 +1,11 @@
 package com.nicholson.client_reservation_vol.domaine.entité
 
 data class Siège(
+    val id : Int,
     val numéro: String,
     val classe: String,
     val statut: String,
-    val avion_id: Int,
+    val idAvion: Int,
     val idRéservation: Int
 )
 
