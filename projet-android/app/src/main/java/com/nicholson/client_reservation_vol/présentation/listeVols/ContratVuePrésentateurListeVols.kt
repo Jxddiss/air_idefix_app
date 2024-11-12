b0c6456c10f30acd131172c2139b83b1fd776284
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-12 17:47:46 -0500

    Conversion de la logique de présentation de la vue liste de vol Conversion au patron MVP

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/listeVols/ContratVuePrésentateurListeVols.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/listeVols/ContratVuePrésentateurListeVols.kt
@@ -1,11 +1,11 @@
 package com.nicholson.client_reservation_vol.présentation.listeVols
 
-import android.view.View
-import com.nicholson.client_reservation_vol.domaine.entité.Vol
+import com.nicholson.client_reservation_vol.présentation.OTD.VolListItemOTD
 
 class ContratVuePrésentateurListeVols {
     interface IListeDeVolsVue {
-        fun afficherVols( listeDeVols : MutableList<Vol> )
+        fun afficherVols( listeDeVols : List<VolListItemOTD> )
+        fun afficherInfoDestination( nomVille: String, urlImage: String )
     }
 
     interface IListeDeVolsPrésentateur {
