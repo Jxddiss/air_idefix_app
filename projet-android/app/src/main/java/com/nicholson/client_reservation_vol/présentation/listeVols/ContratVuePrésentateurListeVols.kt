dd0318a4a4ef92ca803d8423cd69b421d6e59b09
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-15 15:45:54 -0500

    Changement de class à interface pour ContratVuePrésentateurListeVols

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/listeVols/ContratVuePrésentateurListeVols.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/listeVols/ContratVuePrésentateurListeVols.kt
@@ -2,7 +2,7 @@ package com.nicholson.client_reservation_vol.présentation.listeVols
 
 import com.nicholson.client_reservation_vol.présentation.OTD.VolListItemOTD
 
-class ContratVuePrésentateurListeVols {
+interface ContratVuePrésentateurListeVols {
     interface IListeDeVolsVue {
         fun afficherVols( listeDeVols : List<VolListItemOTD> )
         fun afficherInfoDestination( nomVille: String, urlImage: String )
