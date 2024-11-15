3fa68a0271071f601c737e7f5ac277f71693c453
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-15 10:21:48 -0500

    Déplacement de la vue choisir classe vers son package

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/choisirclasse/ChoisirClasseVue.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/choisirclasse/ChoisirClasseVue.kt
@@ -1,4 +1,4 @@
-package com.nicholson.client_reservation_vol.présentation.vue
+package com.nicholson.client_reservation_vol.présentation.choisirclasse
 
 import android.os.Bundle
 import android.view.LayoutInflater
