0633eddeb5e680dd0bd6af3fdab307079dbc3455
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-11-15 15:40:48 -0500

    Changement de type  pour des string représentant le code de l'aéroport au lieu d'un objet Aéroport dans les FiltreRechercheVol

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/OTD/FiltreRechercheVol.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/présentation/OTD/FiltreRechercheVol.kt
@@ -1,10 +1,9 @@
 package com.nicholson.client_reservation_vol.présentation.OTD
 
-import com.nicholson.client_reservation_vol.domaine.entité.Aeroport
 import java.time.LocalDateTime
 
 data class FiltreRechercheVol(
     val dateDébut : LocalDateTime,
-    val aéroportDébut : Aeroport,
-    val aéroportFin : Aeroport
+    val codeAéroportDébut : String,
+    val codeAéroportFin : String
 )
