73e803fe7fd319be1634ed675c95dae913c85ad8
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-12-05 22:44:29 -0500

    Ajouter d'un intercepteur pour le token

--- projet-android/app/src/main/java/com/nicholson/client_reservation_vol/donnée/http/ClientHttp.kt
+++ projet-android/app/src/main/java/com/nicholson/client_reservation_vol/donnée/http/ClientHttp.kt
@@ -3,14 +3,32 @@ package com.nicholson.client_reservation_vol.donnée.http
 import okhttp3.OkHttpClient
 
 
-class ClientHttp : OkHttpClient() {
+class ClientHttp private constructor() {
     companion object {
         @Volatile
-        private var instance : ClientHttp? = null
+        private var instance : OkHttpClient? = null
 
         fun obtenirInstance() =
             instance ?: synchronized(this) {
-                instance ?: ClientHttp().also { instance = it }
+                instance ?: OkHttpClient.Builder().build().also { instance = it }
             }
+
+        fun ajouterToken( token : String ) {
+            synchronized( this ){
+                instance = ajouterIntercepteur( token, instance )
+                    ?: ajouterIntercepteur( token, OkHttpClient.Builder().build() )
+            }
+        }
+
+        private fun ajouterIntercepteur( token : String, client : OkHttpClient? ) : OkHttpClient? {
+            return client?.newBuilder()?.addInterceptor {
+                    chain ->
+                var requête = chain.request()
+                requête = requête.newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed( requête )
+            }?.build()
+        }
     }
 }
